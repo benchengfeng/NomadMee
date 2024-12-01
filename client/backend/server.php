787ed37d14e63a1bea5852d183a981c6947efc64
benchengfeng <amine_bh@outlook.com>
2024-12-01 07:15:08 +0100

    envloader typo

--- client/backend/server.php
+++ client/backend/server.php
@@ -6,7 +6,7 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 // Include the custom .env loader
-require_once __DIR__ . '/env_loader.php';
+require_once __DIR__ . '/env-loader.php';
 
 try {
     // Load environment variables from the .env file
