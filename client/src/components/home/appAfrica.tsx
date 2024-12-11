ba86f23a535a1854d8984ed75fcf4e37f04495a6
benchengfeng <amine_bh@outlook.com>
2024-12-11 02:26:17 +0100

    add african products

--- client/src/components/home/appAfrica.tsx
+++ client/src/components/home/appAfrica.tsx
@@ -1,32 +1,26 @@
 import React from "react";
 
-import image1 from "../../assets/images/modern-design.jpg";
-import image2 from "../../assets/images/clean-design.jpg";
-import image3 from "../../assets/images/great-support.jpg";
-import image4 from "../../assets/images/easy-customise.jpg";
-import image5 from "../../assets/images/unlimited-features.jpg";
-import image6 from "../../assets/images/advanced-option.jpg";
+import image4 from "../../assets/images/africa/products/acajou.jpg";
+import image5 from "../../assets/images/africa/products/arachide.jpg";
+import image6 from "../../assets/images/africa/products/essentialOils.jpg";
+import image7 from "../../assets/images/africa/products/artistana.jpg";
+import image8 from "../../assets/images/africa/products/djembe.jpg";
+import image9 from "../../assets/images/africa/products/karite.jpg";
 
 import { Row, Col, Card } from "antd";
 
 const { Meta } = Card;
 
-function AppAfrica() {
+function AppChina() {
   const hotPicks = [
-    // { title: "Modern Design", image: image1 },
-    // { title: "Clean and Elegant", image: image2 },
-    // { title: "Great Support", image: image3 },
-    { title: "Easy to Customise", image: image4 },
-    { title: "Unlimited Features", image: image5 },
-    { title: "Advanced Options", image: image6 },
+    { title: "cashew nuts", image: image4 },
+    { title: "Peanuts", image: image5 },
+    { title: "Essential Oils", image: image6 },
   ];
   const exclusiveOpps = [
-    { title: "Modern Design", image: image1 },
-    { title: "Clean and Elegant", image: image2 },
-    { title: "Great Support", image: image3 },
-    // { title: "Easy to Customise", image: image4 },
-    // { title: "Unlimited Features", image: image5 },
-    // { title: "Advanced Options", image: image6 },
+    { title: "Artisans", image: image7 },
+    { title: "Djembe Drums", image: image8 },
+    { title: "Shea Butter", image: image9 },
   ];
 
   return (
@@ -41,7 +35,9 @@ function AppAfrica() {
             <Col xs={24} sm={12} md={8} key={index}>
               <Card
                 hoverable
-                cover={<img alt={hotPick.title} src={hotPick.image} />}
+                cover={
+                  <img alt={hotPick.title} src={hotPick.image} />
+                }
                 className="featureCard"
               >
                 <Meta title={hotPick.title} />
@@ -58,7 +54,9 @@ function AppAfrica() {
             <Col xs={24} sm={12} md={8} key={index}>
               <Card
                 hoverable
-                cover={<img alt={exclusiveOpp.title} src={exclusiveOpp.image} />}
+                cover={
+                  <img alt={exclusiveOpp.title} src={exclusiveOpp.image} />
+                }
                 className="featureCard"
               >
                 <Meta title={exclusiveOpp.title} />
@@ -66,10 +64,9 @@ function AppAfrica() {
             </Col>
           ))}
         </Row>
-        
       </div>
     </div>
   );
 }
 
-export default AppAfrica;
+export default AppChina;
