1b5349c166777dc8b693c3f091e00a47965038bb
benchengfeng <amine_bh@outlook.com>
2025-01-02 03:37:09 +0100

    add modals

--- client/src/components/home/homeDeals.tsx
+++ client/src/components/home/homeDeals.tsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Modal, Row, Col, Card } from "antd";
 
 import image1 from "../../assets/images/china/business/businessNegotiation.png";
 import image2 from "../../assets/images/china/business/hotel.jpg";
 import image3 from "../../assets/images/china/business/loadedPort.jpg";
-// import image4 from "../../assets/images/china/products/e-bike.jpeg";
-// import image5 from "../../assets/images/china/products/usbLedLights.jpeg";
-// import image6 from "../../assets/images/china/products/fashionWoman.jpeg";
-// import image7 from "../../assets/images/china/products/motoRainCoat.jpeg";
-// import image8 from "../../assets/images/china/products/engineOilWholesale.jpeg";
-// import image9 from "../../assets/images/china/products/motoSpareParts.jpeg";
-
-import { Row, Col, Card } from "antd";
 
 const { Meta } = Card;
+
+// Define Deal type
+interface Deal {
+  title: string;
+  image: string;
+  description: string;
+}
 
 function HomeDeals() {
   const { t } = useTranslation(); // Initialize t for translations
 
-  const deals = [
-    { title: t("deals.businessNegotiations"), image: image1 },
-    { title: t("deals.hotelAndAirport"), image: image2 },
-    { title: t("deals.guidedBusinessTour"), image: image3 },
+  // State to manage modal visibility and content, typed with the Deal type
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalContent, setModalContent] = useState<Deal | {}>({}); // Initial state is an empty object, but it's of type Deal | {}
+
+  // Deals data including description
+  const deals: Deal[] = [
+    {
+      title: t("deals.businessNegotiations"),
+      image: image1,
+      description: t("deals.businessNegotiationDescription"),
+    },
+    {
+      title: t("deals.hotelAndAirport"),
+      image: image2,
+      description: t("deals.hotelAndAirportDescription"),
+    },
+    {
+      title: t("deals.guidedBusinessTour"),
+      image: image3,
+      description: t("deals.guidedBusinessTourDescription"),
+    },
   ];
-  // const hotPicks = [
-  //   { title: t("hotPicks.eBikes"), image: image4 },
-  //   { title: t("hotPicks.ledLights"), image: image5 },
-  //   { title: t("hotPicks.fashionForWomen"), image: image6 },
-  // ];
-  // const exclusiveOpps = [
-  //   { title: t("exclusiveOpps.motorcycleRainCoats"), image: image7 },
-  //   { title: t("exclusiveOpps.wholesaleEngineOil"), image: image8 },
-  //   { title: t("exclusiveOpps.motorcycleSpareParts"), image: image9 },
-  // ];
+
+  // Function to show the modal with the selected deal
+  const showModal = (deal: Deal) => {
+    setModalContent(deal);
+    setIsModalVisible(true);
+  };
+
+  // Function to handle closing of the modal
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   return (
     <div id="feature" className="featureBlock">
@@ -48,51 +66,26 @@ function HomeDeals() {
                 hoverable
                 cover={<img alt={deal.title} src={deal.image} />}
                 className="featureCard"
+                onClick={() => showModal(deal)} // Open modal on card click
               >
                 <Meta title={deal.title} />
               </Card>
             </Col>
           ))}
         </Row>
-        {/* <div className="titleHolder">
-          <h2>{t("hotPicks.title")}</h2>
-          <p>{t("hotPicks.subtitle")}</p>
-        </div>
-        <Row gutter={[24, 24]}>
-          {hotPicks.map((hotPick, index) => (
-            <Col xs={24} sm={12} md={8} key={index}>
-              <Card
-                hoverable
-                cover={
-                  <img alt={hotPick.title} src={hotPick.image} />
-                }
-                className="featureCard"
-              >
-                <Meta title={hotPick.title} />
-              </Card>
-            </Col>
-          ))}
-        </Row>
-        <div className="titleHolder">
-          <h2>{t("exclusiveOpps.title")}</h2>
-          <p>{t("exclusiveOpps.subtitle")}</p>
-        </div>
-        <Row gutter={[24, 24]}>
-          {exclusiveOpps.map((exclusiveOpp, index) => (
-            <Col xs={24} sm={12} md={8} key={index}>
-              <Card
-                hoverable
-                cover={
-                  <img alt={exclusiveOpp.title} src={exclusiveOpp.image} />
-                }
-                className="featureCard"
-              >
-                <Meta title={exclusiveOpp.title} />
-              </Card>
-            </Col>
-          ))}
-        </Row> */}
       </div>
+
+      {/* Modal to show deal details */}
+      <Modal
+        title={modalContent && 'title' in modalContent ? modalContent.title : ""}
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+        width={600}
+      >
+        <img alt={modalContent && 'title' in modalContent ? modalContent.title : ""} src={modalContent && 'image' in modalContent ? modalContent.image : ""} style={{ width: "100%" }} />
+        <p>{modalContent && 'description' in modalContent ? modalContent.description : ""}</p>
+      </Modal>
     </div>
   );
 }
