cf161862d39ee524c31d688b27aa5fc3dd94ac6e
benchengfeng <amine_bh@outlook.com>
2025-01-04 14:09:29 +0100

    fix ecosystem typo

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'backend-service', // The name of your PM2 service
-      script: './dist/index.js', // The main file of your backend in the dist folder
+      script: './dist/server.js', // The main file of your backend in the dist folder
       watch: ['./dist'], // Watch changes in the dist folder for automatic restarts
       env: {
         NODE_ENV: 'production', // Environment variables for production
