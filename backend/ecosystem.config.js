98a527b6be8058b03f92d84fcf9b62ce6dffd6f0
benchengfeng <amine_bh@outlook.com>
2025-01-04 14:15:59 +0100

    update yaml

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'backend-service', // The name of your PM2 service
+      name: 'server', // The name of your PM2 service
       script: './dist/server.js', // The main file of your backend in the dist folder
       watch: ['./dist'], // Watch changes in the dist folder for automatic restarts
       env: {
